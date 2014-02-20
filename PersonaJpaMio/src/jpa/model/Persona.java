cd35d876dcc3d89f7cf255fa2178d93004563b92
Administrador <Administrador@RAMON04.indra.es>
2014-02-20 09:54:14 +0100

    versión 1

--- PersonaJpaMio/src/jpa/model/Persona.java
+++ PersonaJpaMio/src/jpa/model/Persona.java
@@ -17,6 +17,7 @@ public class Persona implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
+	@GeneratedValue
 	@Column(unique=true, nullable=false)
 	private int id;
 
